d57f00d8c1c1f4b78845ac3302da08de6c460295
johnpaschoud <john@paschoud.org>
2023-06-26 16:55:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,60 @@ input.onGesture(Gesture.Shake, function () {
 })
 let number = 0
 basic.showIcon(IconNames.Heart)
+number = randint(0, 6)
 basic.forever(function () {
-	
+    if (number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (number == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
